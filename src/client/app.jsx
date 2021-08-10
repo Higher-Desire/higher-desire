46b4792a4a2521417be34a111c2601d77e0ea048
Kadenj117 <kadenj117@gmail.com>
2021-08-10 13:59:22 -0600

    started frontend functionality

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+import JobBoard from './components/jobBoard';
+import AppBar from '@material-ui/core/AppBar';
+import { Drawer, Toolbar } from '@material-ui/core';
+
+
 const App = () => {
   return (
     <div>
-      <h2>React Rendered</h2>
-    </div>
+      <AppBar position="fixed">
+        <Toolbar variant='dense'>
+
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" anchor="left">
+        <div>
+          
+        </div>
+      </Drawer>
+      <div style={{marginBottom: '50px'}}></div>
+      <JobBoard />
+    </div >
   )
 }
 
