24689d7ff2b6a0b84d9b4e1938695980a28c24b4
Fahad Shaikh <fahad.87.shaikh@icloud.com>
2021-08-11 12:58:37 -0700

    premerge

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
-
-import JobBoard from './components/jobBoard';
-import AppBar from '@material-ui/core/AppBar';
-import { Drawer, Toolbar } from '@material-ui/core';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Signin from './components/signin';
+import Signup from './components/signup';
 
 const App = () => {
   return (
-    <div>
-      <AppBar position="fixed">
-        <Toolbar variant='dense'>
-
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" anchor="left">
-        <div>
-          
-        </div>
-      </Drawer>
-      <div style={{marginBottom: '50px'}}></div>
-      <JobBoard />
+    <Router>
+    <div className ="container">
+      <h1>Adding Routes!</h1>
     </div >
+    <Route path="/" component={Signin}></Route>
+    <Route path="/signup" component={Signup}></Route>
+    </Router>
   )
 }
 
