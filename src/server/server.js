3097491e0b4b43d48e7ebf4dcd4271c1c6c33b7d
Kadenj117 <kadenj117@gmail.com>
2021-08-12 10:06:00 -0600

    fixed login and switches

--- src/server/server.js
+++ src/server/server.js
@@ -4,8 +4,11 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(express.json())
-console.log('')
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../client/index.html')));
+app.get('/signup', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../client/index.html')));
+app.get('/board', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../client/index.html')));
+app.get('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../client/index.html')));
+
+app.use((req, res) =>res.sendStatus(404))
 
 app.listen(PORT, () => console.log('listening on port:', PORT));
